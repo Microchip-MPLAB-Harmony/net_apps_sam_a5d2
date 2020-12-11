eb9fe19ba8880945dfdb00fca349ac56004039c7
Adrian Aur <Adrian.Aur@microchip.com>
2020-12-11 15:35:01 -0700

    [MH3-46412] [MH3-46413]- Regeneration and test of the wolfssl_tcp_client SAMA5D2 FreeRTOS demo

--- apps/wolfssl_tcp_client/firmware/src/config/sam_a5d2_xplained_ultra_rtos/FreeRTOSConfig.h
+++ apps/wolfssl_tcp_client/firmware/src/config/sam_a5d2_xplained_ultra_rtos/FreeRTOSConfig.h
@@ -50,7 +50,7 @@
 #define configMINIMAL_STACK_SIZE                ( 1024 )
 #define configSUPPORT_DYNAMIC_ALLOCATION        1
 #define configSUPPORT_STATIC_ALLOCATION         0
-#define configTOTAL_HEAP_SIZE                   ( ( size_t ) 100960 )
+#define configTOTAL_HEAP_SIZE                   ( ( size_t ) 240960 )
 #define configMAX_TASK_NAME_LEN                 ( 16 )
 #define configUSE_16_BIT_TICKS                  0
 #define configIDLE_SHOULD_YIELD                 1
