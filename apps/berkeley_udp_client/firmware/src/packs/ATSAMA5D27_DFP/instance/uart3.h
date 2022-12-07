5384f0029dd52dcf6830017fca32600e04a009aa
Adrian Aur <Adrian.Aur@microchip.com>
2022-12-07 15:43:10 -0700

    [MH3-72510], [MH3-72512] - Regenerated berkeley_udp_client application

--- apps/berkeley_udp_client/firmware/src/packs/ATSAMA5D27_DFP/instance/uart3.h
+++ apps/berkeley_udp_client/firmware/src/packs/ATSAMA5D27_DFP/instance/uart3.h
@@ -20,17 +20,17 @@
  *
  */
 
-/* file generated from device description version 2021-12-23T10:17:04Z */
+/* file generated from device description version 2022-02-21T21:02:22Z */
 #ifndef _SAMA5D2_UART3_INSTANCE_
 #define _SAMA5D2_UART3_INSTANCE_
 
 
 /* ========== Instance Parameter definitions for UART3 peripheral ========== */
-#define UART3_INSTANCE_ID                        _UL_(27)   
-#define UART3_CLOCK_ID                           _UL_(27)   
-#define UART3_DMAC_ID_TX                         _UL_(41)   
-#define UART3_DMAC_ID_RX                         _UL_(42)   
-#define UART3_BRSRCCK_PERIPH_CLK                 _UL_(0)    /* MCK */
-#define UART3_BRSRCCK_GCLK                       _UL_(0)    /* GCLK */
+#define UART3_INSTANCE_ID                        (27)       
+#define UART3_CLOCK_ID                           (27)       
+#define UART3_DMAC_ID_TX                         (41)       
+#define UART3_DMAC_ID_RX                         (42)       
+#define UART3_BRSRCCK_PERIPH_CLK                 (0)        /* MCK */
+#define UART3_BRSRCCK_GCLK                       (0)        /* GCLK */
 
 #endif /* _SAMA5D2_UART3_INSTANCE_ */
