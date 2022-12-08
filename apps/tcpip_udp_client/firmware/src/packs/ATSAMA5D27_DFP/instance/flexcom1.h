31e5d3234717327c09b3669bd493d990ad9edc5c
Adrian Aur <Adrian.Aur@microchip.com>
2022-12-08 09:10:52 -0700

    [MH3-72524], [MH3-72525] - Regenerated tcpip_udp_client application

--- apps/tcpip_udp_client/firmware/src/packs/ATSAMA5D27_DFP/instance/flexcom1.h
+++ apps/tcpip_udp_client/firmware/src/packs/ATSAMA5D27_DFP/instance/flexcom1.h
@@ -20,18 +20,18 @@
  *
  */
 
-/* file generated from device description version 2021-12-23T10:17:04Z */
+/* file generated from device description version 2022-02-21T21:02:22Z */
 #ifndef _SAMA5D2_FLEXCOM1_INSTANCE_
 #define _SAMA5D2_FLEXCOM1_INSTANCE_
 
 
 /* ========== Instance Parameter definitions for FLEXCOM1 peripheral ========== */
-#define FLEXCOM1_INSTANCE_ID                     _UL_(20)   
-#define FLEXCOM1_CLOCK_ID                        _UL_(20)   
-#define FLEXCOM1_DMAC_ID_TX                      _UL_(13)   
-#define FLEXCOM1_DMAC_ID_RX                      _UL_(14)   
-#define FLEXCOM1_USART_FIFO_SIZE                 _UL_(32)   
-#define FLEXCOM1_SPI_FIFO_SIZE                   _UL_(32)   
-#define FLEXCOM1_TWI_FIFO_SIZE                   _UL_(16)   
+#define FLEXCOM1_INSTANCE_ID                     (20)       
+#define FLEXCOM1_CLOCK_ID                        (20)       
+#define FLEXCOM1_DMAC_ID_TX                      (13)       
+#define FLEXCOM1_DMAC_ID_RX                      (14)       
+#define FLEXCOM1_USART_FIFO_SIZE                 (32)       
+#define FLEXCOM1_SPI_FIFO_SIZE                   (32)       
+#define FLEXCOM1_TWI_FIFO_SIZE                   (16)       
 
 #endif /* _SAMA5D2_FLEXCOM1_INSTANCE_ */
--- apps/tcpip_udp_client/firmware/src/packs/ATSAMA5D27_DFP/instance/pit.h
+++ apps/tcpip_udp_client/firmware/src/packs/ATSAMA5D27_DFP/instance/pit.h
@@ -20,12 +20,12 @@
  *
  */
 
-/* file generated from device description version 2021-12-23T10:17:04Z */
+/* file generated from device description version 2022-02-21T21:02:22Z */
 #ifndef _SAMA5D2_PIT_INSTANCE_
 #define _SAMA5D2_PIT_INSTANCE_
 
 
 /* ========== Instance Parameter definitions for PIT peripheral ========== */
-#define PIT_INSTANCE_ID                          _UL_(3)    
+#define PIT_INSTANCE_ID                          (3)        
 
 #endif /* _SAMA5D2_PIT_INSTANCE_ */
