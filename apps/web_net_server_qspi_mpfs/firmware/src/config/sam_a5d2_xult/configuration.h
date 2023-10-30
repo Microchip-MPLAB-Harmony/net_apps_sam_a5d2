9d9cd29d60be6ef6142bffd1af917079dbab3d36
hrisikesh sahu <hrisikesh.sahu@microchip.com>
2023-10-30 10:23:49 +0530

    [MH3-83767] web_net_server_qspi_mpfs application regeneration - SAMA5D2

--- apps/web_net_server_qspi_mpfs/firmware/src/config/sam_a5d2_xult/configuration.h
+++ apps/web_net_server_qspi_mpfs/firmware/src/config/sam_a5d2_xult/configuration.h
@@ -444,7 +444,7 @@ extern "C" {
 
 /*** TCPIP Heap Configuration ***/
 #define TCPIP_STACK_USE_INTERNAL_HEAP
-#define TCPIP_STACK_DRAM_SIZE                       102400
+#define TCPIP_STACK_DRAM_SIZE                       182400
 #define TCPIP_STACK_DRAM_RUN_LIMIT                  2048
 
 #define TCPIP_STACK_MALLOC_FUNC                     malloc
