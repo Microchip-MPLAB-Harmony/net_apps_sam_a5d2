2033ff0d224fbc08cd17c3093b531c5db5bca1e1
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2024-04-05 12:12:23 +0530

    [MH3-90122] - Regenerated iperf_demo application - sam_a5d2_curiosity_freertos

--- apps/iperf_demo/firmware/src/config/sam_a5d2_curiosity_freertos/definitions.h
+++ apps/iperf_demo/firmware/src/config/sam_a5d2_curiosity_freertos/definitions.h
@@ -49,6 +49,7 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include "crypto/crypto.h"
+#include "bsp/bsp.h"
 #include "peripheral/pit/plib_pit.h"
 #include "peripheral/tc/plib_tc0.h"
 #include "system/time/sys_time.h"
